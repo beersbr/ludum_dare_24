94fb303cd7b0ce959fd4b1bae9414ebd1bf6e5ed
Brett Beers <beersbr@gmail.com>
2012-08-24 19:54:54 -0600

    first commit !!!

---
+++ main.py
@@ -0,0 +1,73 @@
+import pygame, os, sys
+from pygame.locals import *
+
+# ##########################################################
+# GameInput class
+# This object will take care of handling keyboard events.
+# TODO: make this a singleton as I dont want more than one instance... or does it really matter?
+# ##########################################################
+class GameInput():
+	def __init__(self):
+		self._keys = {}
+
+	def consume(self, event):
+		if event.type == pygame.KEYDOWN:
+			self._keys[event.key] = True
+		if event.type == pygame.KEYUP:
+			self._keys[event.key] = False
+
+	def key_down(self, key):
+		if key in self._keys.keys():
+			if self._keys[key] == True:
+				return True
+		return False
+
+# ##########################################################
+# Game class 
+# This object will take care of maintaining the game state
+# ##########################################################
+class Game():
+	def __init__(self, res_x = 800, res_y = 600, fullscreen = False):
+		self._res = self._res_x, self._res_y = res_x, res_y
+		self.running = True
+		self.input = GameInput()
+
+	def game_init(self):
+		pygame.init()
+		pygame.display.set_caption('LudumDare 24 :: Evolution')
+		self.canvas = pygame.display.set_mode(self._res, pygame.HWSURFACE | pygame.DOUBLEBUF)
+
+	def draw(self):
+		pass
+
+
+	def update(self):
+		pass
+
+	def cleanup(self):
+		pygame.quit()
+
+	def gather_events(self, event):
+		if event.type == pygame.QUIT:
+			self.running = False
+		
+		self.input.consume(event)
+
+	def play(self):
+		if self.game_init() == False:
+			self.running = False
+
+		while self.running:
+			for event in pygame.event.get():
+				self.gather_events(event)
+
+			self.update()
+			self.canvas.fill(pygame.Color("Black"))
+			self.draw()
+			pygame.display.flip()
+
+		self.cleanup()
+
+if __name__ == "__main__":
+	game = Game()
+	game.play()
